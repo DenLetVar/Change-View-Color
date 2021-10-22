ba9f740d9eef91d6134a78a13b87bb070c8a6bf4
DenLetVar <denletvar@gmail.com>
2021-10-23 00:13:15 +0300

    Create app change view color

--- Change View Color/ViewController.swift
+++ Change View Color/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var colorView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLabel.text = String(redSlider.value)
+        greenLabel.text = String(greenSlider.value)
+        blueLabel.text = String(blueSlider.value)
+        
     }
 
-
+    @IBAction func redSliderAction(_ sender: Any) {
+        redLabel.text = String(round(redSlider.value * 100) / 100)
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
+    @IBAction func greenSliderAction(_ sender: Any) {
+        greenLabel.text = String(round(greenSlider.value * 100) / 100)
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
+    @IBAction func blueSliderAction(_ sender: Any) {
+        blueLabel.text = String(round(blueSlider.value * 100) / 100)
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
+    
+    
+    
 }
 
